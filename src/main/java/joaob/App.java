3b56e2e70e038a550a01a918c74b153ea3d108ab
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2023-11-12 22:57:52 +0000

    site

--- src/main/java/joaob/App.java
+++ src/main/java/joaob/App.java
@@ -6,16 +6,21 @@ public class App
     private static final int ID = 1;
     public static void main( String[] args )
     {
-
-        APIHandler API_HANDLER = new APIHandler(ID);
-
-        API_HANDLER.loadData();
-
-        System.out.println(API_HANDLER.getData());
         try{
-        API_HANDLER.saveToFile(Integer.toString(ID));
+
+            APIHandler API_HANDLER = new APIHandler(ID);
+
+            API_HANDLER.loadData();
+
+            System.out.println(API_HANDLER.getData());
+            
+            API_HANDLER.saveToFile(Integer.toString(ID));
+
+
         }catch(Exception e){
             System.out.println(e);
         }
+
+
     }
 }
